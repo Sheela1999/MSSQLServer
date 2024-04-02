3c96b8231dfc755f24119d75567d3c1874ec8a4f
Sheela1999 <sheela.xworkz@gmail.com>
2024-04-02 16:42:08 +0530

    Emp

---
+++ Employee.sql
@@ -0,0 +1,9 @@
+create database dotNet;
+use dotNet;
+
+create table Employee (id int, name varchar(40), age int, salary int, department varchar(40));
+
+insert into Employee values(1, 'Maha', 21, 33000, 'MES'); 
+insert into Employee values(2, 'Moulya', 23, 38000, 'SalesForce');
+insert into Employee values(3, 'Sheela', 25, 20000, 'Java Developer');
+select * from Employee;
