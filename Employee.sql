d420932bbf98596234e9ae077c618efcbc171418
Sheela1999 <sheela.xworkz@gmail.com>
2024-05-07 23:41:30 +0530

    Student details

--- Employee.sql
+++ Employee.sql
@@ -6,4 +6,5 @@ create table Employee (id int, name varchar(40), age int, salary int, department
 insert into Employee values(1, 'Maha', 21, 33000, 'MES'); 
 insert into Employee values(2, 'Moulya', 23, 38000, 'SalesForce');
 insert into Employee values(3, 'Sheela', 25, 20000, 'Java Developer');
+insert into Employee values(4, 'Bharati', 22, 15000, 'Manual Testing');
 select * from Employee;
---
+++ Student.sql
@@ -0,0 +1,9 @@
+use dotNet;
+
+create table Student(id int, name varchar(40), class int, place varchar(40), age int);
+insert into Student values(1, 'Shri', 13, 'Koppala', 18);
+insert into Student values(2, 'Shilpa', 12, 'Bagalkot', 16);
+insert into Student values(3, 'Pooja', 11, 'Belagavi', 17);
+insert into Student values(4, 'Ananya', 8, 'Bangalore', 14);
+Select * from Student;
+
